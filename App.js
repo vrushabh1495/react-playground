2a0dfe128fa4d2ea8a87c079f89c19b786f822de
Vrushabh Chougale <vchougal@akamai.com>
2024-11-13 08:15:15 +0530

    Laying the foundation

--- App.js
+++ App.js
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const heading = React.createElement("h1", {id:"heading"}, "hello world from react");
+const Title = () => (
+    <h1 id="title"> Some title</h1>
+)
+
+const number = 1000
+const HeadingComponent = () => (
+    <div id="container">
+        <Title/>
+        {number}
+        <h1 id="heading">
+            Some heading
+        </h1>
+    </div>
+)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingComponent/>);
