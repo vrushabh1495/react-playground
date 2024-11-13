be4034c93a02f5b417c8e18d334bb5a971cbdb50
Vrushabh Chougale <vchougal@akamai.com>
2024-11-13 06:24:31 +0530

    Igniting the app

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const heading = React.createElement("h1", {id:"heading"}, "hello world from react");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
